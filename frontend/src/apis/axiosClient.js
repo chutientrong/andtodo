bd6c8aeb36d6a7aca2347284c44053a397761b0c
chutientrong <trongct@olabs.net>
2024-06-04 22:02:27 +0700

    update: nginx

--- frontend/src/apis/axiosClient.js
+++ frontend/src/apis/axiosClient.js
@@ -1,59 +1,56 @@
-import axios from 'axios';
-import queryString from 'query-string';
-import { routes } from '../router';
+import axios from "axios";
+import queryString from "query-string";
+import { routes } from "../router";
 
 const axiosClient = axios.create({
-    baseURL: process.env.REACT_APP_API_URL,
-    headers: {
-        'content-type': 'application/json',
-    },
-    paramsSerializer: (params) => queryString.stringify(params),
+  baseURL: process.env.REACT_APP_API_URL,
+  headers: {
+    "content-type": "application/json",
+  },
+  paramsSerializer: (params) => queryString.stringify(params),
 });
 
 axiosClient.interceptors.request.use(async (config) => {
-    const token = localStorage.getItem('token');
+  const token = localStorage.getItem("token");
 
-    if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
 });
 
 axiosClient.interceptors.response.use(
-    (response) => {
-        // if (response && response.data) {
-        //     return response.data;
-        // }
+  (response) => {
+    // if (response && response.data) {
+    //     return response.data;
+    // }
 
-        return response;
-    },
-    async (error) => {
-        const refreshToken = localStorage.getItem('refreshToken');
-        if (error.response.status !== 401) {
-           
-            return Promise.reject(error);
-        }
-        console.log('error: ' + error.response.status);
-        axios.interceptors.response.eject(axiosClient.interceptors);
-        return axios
-            .post('/auth/refresh-token', {
-                refreshToken,
-            })
-            .then((token) => {
-                localStorage.setItem('token', token);
-                error.response.config.headers['Authorization'] =
-                    'Bearer ' + token;
-                return axios(error.response.config);
-            })
-            .catch((error) => {
-                localStorage.removeItem('token');
-                localStorage.removeItem('refreshToken');
-                window.location.href = `${routes.LOGIN}`
-                console.log('error', error);
-                return Promise.reject(error);
-            })
-            .finally();
+    return response;
+  },
+  async (error) => {
+    const refreshToken = localStorage.getItem("refreshToken");
+    if (error && error.response && error.response.status !== 401) {
+      return Promise.reject(error);
     }
+    axios.interceptors.response.eject(axiosClient.interceptors);
+    return axios
+      .post("/auth/refresh-token", {
+        refreshToken,
+      })
+      .then((token) => {
+        localStorage.setItem("token", token);
+        error.response.config.headers["Authorization"] = "Bearer " + token;
+        return axios(error.response.config);
+      })
+      .catch((error) => {
+        localStorage.removeItem("token");
+        localStorage.removeItem("refreshToken");
+        window.location.href = `${routes.LOGIN}`;
+        console.log("error", error);
+        return Promise.reject(error);
+      })
+      .finally();
+  }
 );
 
 export default axiosClient;
